a6bf47ec98b03e640301c93d4412b865663efa93
Polvere243 <danielercoli@gmail.com>
2024-10-14 15:08:16 +0200

    made the toggle work

--- app.js
+++ app.js
@@ -3,3 +3,19 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+// recupero gli elementi dal DOM
+
+const navToggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links');
+
+navToggle.addEventListener('click', () => {
+    //    stabilisco una condizione per aggiungere o togliere la classe che mostra i link
+    if (links.classList.contains('show-links')) {
+        links.classList.remove('show-links');
+    } else {
+        links.classList.add('show-links');
+    }
+
+
+});
